b30183453d7e0b8811280c0332cb82dbe8ef676d
jaredad <35081849+jaredad@users.noreply.github.com>
2018-02-25 18:00:12 -0600

    Add files via upload

---
+++ Hendrix-Hookups-352-master/Hendrix-Hookups-352-master/HH/src/functionality/Profile.java
@@ -0,0 +1,122 @@
+package functionality;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+
+public class Profile {
+	
+	private String name;
+	private String ip;
+	private String stat;
+	private String bio;
+	private Image pic;
+	private ProfileList accepted;
+	private File file;
+	private File imageFile;
+	private String line;
+	
+	public Profile(String user_name, String ip_address, String status, String biography, Image picture, ProfileList list) {
+		name = user_name;
+		ip = ip_address;
+		stat = status;
+		bio = biography;
+		pic = picture;
+		accepted = list;
+	}
+	
+	public void editProfile(String n, String s, String b, Image p, ProfileList l) {
+		name = n;
+		stat = s;
+		bio = b;
+		pic = p;
+		accepted = l;
+	}
+	
+	public void createProfile() {
+		file = new File(System.getProperty("user.dir")+"\\"+name+".txt");
+		try {
+			file.createNewFile();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void saveProfile() {
+		//TODO figure out how to save image
+		FileWriter writer = null;
+		try {
+			System.out.println(file.getAbsolutePath());
+			writer = new FileWriter(file.getAbsolutePath());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("made it");
+		PrintWriter print = new PrintWriter(writer);
+		print.println("Hendrix-Hookups File");
+		print.println(name);
+		print.println(ip);
+		print.println(stat);
+		print.println(bio);	
+		print.close();
+	}
+	
+	public void loadFile(TextArea bio, TextField name, TextField status, File file) {
+		try {
+			FileReader fileReader = new FileReader(file.getAbsolutePath());
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			try {
+				line = bufferedReader.readLine();
+				if (line.equals("Hendrix-Hookups File")) {
+					name.setText(bufferedReader.readLine());
+					bufferedReader.readLine();
+					status.setText(bufferedReader.readLine());
+					bio.setText(bufferedReader.readLine());
+					//TODO open image
+				} else {
+					//TODO
+				}
+			} catch (IOException e) {
+				//TODO
+			}
+		} catch (FileNotFoundException e) {
+			//TODO
+		}
+	}
+	
+	public String getIp() {
+		return ip;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getStat() {
+		return stat;
+	}
+	
+	public String getBio() {
+		return bio;
+	}
+	
+	public Image getPic() {
+		return pic;
+	}
+	
+	public ProfileList getList() {
+		return accepted;
+	}
+	
+	public void setList(ProfileList list) {
+		accepted = list;
+	}
+}
---
+++ Hendrix-Hookups-352-master/Hendrix-Hookups-352-master/HH/src/functionality/Request.java
@@ -0,0 +1,48 @@
+package functionality;
+
+import java.util.Optional;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
+
+public class Request {
+	
+	private String info;
+	
+	public String getInfo(Profile p) {
+		info = "You have recieved a request from "+p.getName()+".\n"+
+				"Status: " + p.getStat()+"\n"+
+				"Bio: " + p.getBio()+"\n";
+		return info;
+		}
+	
+	public void sendRequest(Profile a, Profile b) {
+		//TODO implement a way to send to the second profile
+		Alert alert = new Alert(AlertType.CONFIRMATION, getInfo(a), ButtonType.YES,ButtonType.NO);
+		alert.setWidth(200);
+		alert.setHeight(200);
+		Optional<ButtonType> result = alert.showAndWait();
+		if(!result.isPresent()) {
+			return;
+		}
+		else if(result.get() == ButtonType.YES) {
+			acceptRequest(a,b);
+		}
+		else if(result.get() == ButtonType.NO) {
+			return;
+		}
+	}
+	
+	public void acceptRequest(Profile a, Profile b) {
+		ProfileList list_a = a.getList();
+		list_a.addProfile(b);
+		a.setList(list_a);
+		ProfileList list_b = b.getList();
+		list_b.addProfile(a);
+		b.setList(list_b);
+	}
+	
+	
+
+}
